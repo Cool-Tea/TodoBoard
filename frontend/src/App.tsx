02a260efbba463810ce330a9a3455e26a6bdb836
cool-tea <943801990@qq.com>
2024-07-29 14:56:03 +0000

    Resume to scratch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { Workspace } from "./pages/Workspace/Workspace";
-import { Login } from "./pages/Login/Login";
-import { Register } from "./pages/Register/Register";
-import { Repository } from "./pages/Repository/Repository";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { TaskPage } from "./pages/Task/TaskPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
 
@@ -12,13 +7,6 @@ export default function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate replace to="/login" />} />
-          <Route path="/login" Component={Login} />
-          <Route path="/register" Component={Register} />
-          <Route path="/:uuid/workspace" Component={Repository} />
-          <Route path="/:uuid/workspace/:wuid" Component={Workspace} />
-          <Route path="/:uuid/task/:tuid" Component={TaskPage} />
-          <Route path="*" element={<Navigate replace to="/login" />} />
         </Routes>
       </BrowserRouter>
     </div>
