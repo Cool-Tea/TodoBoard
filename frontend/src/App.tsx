fd8e735378e6ce07f43e4b2b783936aa96506a90
cool-tea <943801990@qq.com>
2024-07-30 09:21:03 +0000

    Add login and register layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Task } from "./pages/Task";
 import { Project } from "./pages/Project";
 import { Repository } from "./pages/Repository";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 
 export default function App() {
 
@@ -14,6 +16,8 @@ export default function App() {
           <Route path="/task" Component={Task} /> 
           <Route path="/project" Component={Project} /> 
           <Route path="/repository" Component={Repository} /> 
+          <Route path="/login" Component={Login} />
+          <Route path="/register" Component={Register} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import reactIcon from "../assets/react.svg"
+
+export function Register() {
+
+  return (
+    <div className="relative bg-sky-50 min-h-screen flex justify-center items-center">
+      <div className="bg-white p-4 space-y-4 rounded-lg ring-1 ring-gray-900/5 shadow-lg flex flex-col">
+        <div className="flex flex-row items-center space-x-4">
+          <img src={reactIcon} className="w-12 h-12" />
+          <p className="font-serif text-2xl font-bold">Register</p>
+        </div>
+        <form id="loginForm" className="text-lg px-2 space-y-2">
+          New User Name: <input type="text" className="px-2 rounded-lg ring-1 ring-gray-900/50" /> <br />
+          Set Password: <input type="text" className="px-2 rounded-lg ring-1 ring-gray-900/50"/> <br />
+          <div className="flex justify-between">
+            <button type="button" className="text-sky-600 hover:text-sky-800">Back</button>
+            <button type="button" className="text-white px-2 py-1 rounded-lg bg-sky-600 hover:bg-sky-800">Register</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
