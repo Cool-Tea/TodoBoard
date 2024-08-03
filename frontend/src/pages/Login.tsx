0d80780788166f155957a11fba840f0a6c3e7417
cool-tea <943801990@qq.com>
2024-08-03 20:05:42 +0000

    Add login and register operation

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import reactIcon from "../assets/react.svg"
 import { useNavigate } from "react-router";
+import * as axios from "axios"
 
 export function Login() {
   const navigate = useNavigate();
+  const client = axios.default;
+  const [reason, setReason] = useState<string | null>(null);
+
+  function login() {
+    event?.preventDefault();
+    const login = document.getElementById('login') as HTMLFormElement;
+    let formData = new FormData(login);
+    let data = {
+      user: formData.get('name') as string,
+      password: formData.get('password') as string,
+    };
+    if (data.user.includes(' ') || data.password.includes(' ')) {
+      setReason('Invalid user name or password');
+      return;
+    }
+    client.post("http://127.0.0.1:7001/user/login", data).then(res => {
+      let body = res.data;
+      if (!body.success) {
+        console.log(`Error: ${body.reason}`);
+        setReason(body.reason);
+      }
+      else {
+        navigate(`/${data.user}/repository`);
+      }
+    }).catch(err => console.log(`Error: ${err}`));
+  }
 
   return (
     <div className="relative bg-sky-50 min-h-screen flex justify-center items-center">
@@ -12,12 +39,18 @@ export function Login() {
           <img src={reactIcon} className="w-12 h-12" />
           <p className="font-serif text-2xl font-bold">Login</p>
         </div>
-        <form id="loginForm" className="text-lg px-2 space-y-2">
-          User Name: <input type="text" className="px-2 rounded-lg ring-1 ring-gray-900/50" /> <br />
-          Password: <input type="text" className="px-2 rounded-lg ring-1 ring-gray-900/50"/> <br />
+        <form id="login" className="text-lg px-2 space-y-2">
+          User Name: <input type="text" name="name" required className="px-2 rounded-lg ring-1 ring-gray-900/50" /> <br />
+          Password: <input type="password" name="password" required className="px-2 rounded-lg ring-1 ring-gray-900/50"/> <br />
+          {
+            reason && 
+            <div className="text-red-600 text-sm">
+              {reason}
+            </div>
+          }
           <div className="flex justify-between">
             <button type="button" onClick={()=>navigate('/register')} className="text-sky-600 hover:text-sky-800">Register</button>
-            <button type="button" className="text-white px-2 py-1 rounded-lg bg-sky-600 hover:bg-sky-800">Login</button>
+            <button type="button" onClick={login} className="text-white px-2 py-1 rounded-lg bg-sky-600 hover:bg-sky-800">Login</button>
           </div>
         </form>
       </div>
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import reactIcon from "../assets/react.svg"
 import { useNavigate } from "react-router";
+import * as axios from "axios"
 
 export function Register() {
   const navigate = useNavigate();
+  const client = axios.default;
+  const [reason, setReason] = useState<string | null>(null);
+
+  function register() {
+    event?.preventDefault();
+    const register = document.getElementById('register') as HTMLFormElement;
+    let formData = new FormData(register);
+    let data = {
+      name: formData.get('name') as string,
+      password: formData.get('password') as string,
+    };
+    if (data.name.includes(' ') || data.password.includes(' ')) {
+      setReason('Invalid user name or password (blank not allowed)');
+      return;
+    }
+    client.post("http://127.0.0.1:7001/user/register", data).then(res => {
+      let body = res.data;
+      if (!body.success) {
+        console.log(`Error: ${body.reason}`);
+        setReason(body.reason);
+      }
+      else {
+        navigate('/login');
+      }
+    }).catch(err => console.log(`Error: ${err}`));
+  }
 
   return (
     <div className="relative bg-sky-50 min-h-screen flex justify-center items-center">
@@ -12,12 +39,18 @@ export function Register() {
           <img src={reactIcon} className="w-12 h-12" />
           <p className="font-serif text-2xl font-bold">Register</p>
         </div>
-        <form id="loginForm" className="text-lg px-2 space-y-2">
-          New User Name: <input type="text" className="px-2 rounded-lg ring-1 ring-gray-900/50" /> <br />
-          Set Password: <input type="text" className="px-2 rounded-lg ring-1 ring-gray-900/50"/> <br />
+        <form id="register" className="text-lg px-2 space-y-2">
+          New User Name: <input type="text" name="name" required className="px-2 rounded-lg ring-1 ring-gray-900/50" /> <br />
+          Set Password: <input type="text" name="password" required className="px-2 rounded-lg ring-1 ring-gray-900/50"/> <br />
+          {
+            reason && 
+            <div className="text-red-600 text-sm">
+              {reason}
+            </div>
+          }
           <div className="flex justify-between">
             <button type="button" onClick={()=>navigate('/login')} className="text-sky-600 hover:text-sky-800">Back</button>
-            <button type="button" className="text-white px-2 py-1 rounded-lg bg-sky-600 hover:bg-sky-800">Register</button>
+            <button type="button" onClick={register} className="text-white px-2 py-1 rounded-lg bg-sky-600 hover:bg-sky-800">Register</button>
           </div>
         </form>
       </div>
